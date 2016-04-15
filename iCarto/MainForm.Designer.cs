72b0024405724a5684e53ea19d3724a331cdf3b4
uptonking <jinyaoo86@gmail.com>
2016-04-16 02:02:53 +0800

    首页地图示例

--- iCarto/MainForm.Designer.cs
+++ iCarto/MainForm.Designer.cs
@@ -28,51 +28,308 @@ namespace iCarto
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
-            this.lollipopCard1 = new LollipopCard();
-            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.lollipopLabel1 = new LollipopLabel();
+            this.lollipopLabel2 = new LollipopLabel();
+            this.lollipopLabel3 = new LollipopLabel();
+            this.lollipopLabel4 = new LollipopLabel();
+            this.lollipopLabel5 = new LollipopLabel();
+            this.lollipopLabel6 = new LollipopLabel();
+            this.lollipopLabel7 = new LollipopLabel();
+            this.lollipopLabel8 = new LollipopLabel();
+            this.lollipopLabel9 = new LollipopLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             this.SuspendLayout();
             // 
-            // axMapControl1
+            // lollipopLabel1
             // 
-            this.axMapControl1.Location = new System.Drawing.Point(422, 122);
-            this.axMapControl1.Name = "axMapControl1";
-            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
-            this.axMapControl1.Size = new System.Drawing.Size(281, 298);
-            this.axMapControl1.TabIndex = 0;
+            this.lollipopLabel1.AutoSize = true;
+            this.lollipopLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel1.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel1.Location = new System.Drawing.Point(28, 90);
+            this.lollipopLabel1.Name = "lollipopLabel1";
+            this.lollipopLabel1.Size = new System.Drawing.Size(92, 18);
+            this.lollipopLabel1.TabIndex = 0;
+            this.lollipopLabel1.Text = "最热地图模板";
             // 
-            // lollipopCard1
+            // lollipopLabel2
             // 
-            this.lollipopCard1.ButtonColor = "#33b679";
-            this.lollipopCard1.ContentColor = "#444444";
-            this.lollipopCard1.ContentText = "Card Content is here";
-            this.lollipopCard1.Image = null;
-            this.lollipopCard1.Location = new System.Drawing.Point(57, 122);
-            this.lollipopCard1.Name = "lollipopCard1";
-            this.lollipopCard1.Size = new System.Drawing.Size(294, 298);
-            this.lollipopCard1.TabIndex = 1;
-            this.lollipopCard1.Text = "lollipopCard1";
-            this.lollipopCard1.TitleColor = "#33b679";
+            this.lollipopLabel2.AutoSize = true;
+            this.lollipopLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel2.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel2.Location = new System.Drawing.Point(170, 90);
+            this.lollipopLabel2.Name = "lollipopLabel2";
+            this.lollipopLabel2.Size = new System.Drawing.Size(92, 18);
+            this.lollipopLabel2.TabIndex = 1;
+            this.lollipopLabel2.Text = "最新地图模板";
+            // 
+            // lollipopLabel3
+            // 
+            this.lollipopLabel3.AutoSize = true;
+            this.lollipopLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel3.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel3.Location = new System.Drawing.Point(310, 90);
+            this.lollipopLabel3.Name = "lollipopLabel3";
+            this.lollipopLabel3.Size = new System.Drawing.Size(106, 18);
+            this.lollipopLabel3.TabIndex = 2;
+            this.lollipopLabel3.Text = "基础测绘类地图";
+            // 
+            // lollipopLabel4
+            // 
+            this.lollipopLabel4.AutoSize = true;
+            this.lollipopLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel4.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel4.Location = new System.Drawing.Point(490, 89);
+            this.lollipopLabel4.Name = "lollipopLabel4";
+            this.lollipopLabel4.Size = new System.Drawing.Size(78, 18);
+            this.lollipopLabel4.TabIndex = 3;
+            this.lollipopLabel4.Text = "国土类地图";
+            // 
+            // lollipopLabel5
+            // 
+            this.lollipopLabel5.AutoSize = true;
+            this.lollipopLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel5.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel5.Location = new System.Drawing.Point(634, 89);
+            this.lollipopLabel5.Name = "lollipopLabel5";
+            this.lollipopLabel5.Size = new System.Drawing.Size(78, 18);
+            this.lollipopLabel5.TabIndex = 4;
+            this.lollipopLabel5.Text = "交通类地图";
+            // 
+            // lollipopLabel6
+            // 
+            this.lollipopLabel6.AutoSize = true;
+            this.lollipopLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel6.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel6.Location = new System.Drawing.Point(789, 89);
+            this.lollipopLabel6.Name = "lollipopLabel6";
+            this.lollipopLabel6.Size = new System.Drawing.Size(78, 18);
+            this.lollipopLabel6.TabIndex = 5;
+            this.lollipopLabel6.Text = "规划类地图";
+            // 
+            // lollipopLabel7
+            // 
+            this.lollipopLabel7.AutoSize = true;
+            this.lollipopLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel7.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel7.Location = new System.Drawing.Point(940, 89);
+            this.lollipopLabel7.Name = "lollipopLabel7";
+            this.lollipopLabel7.Size = new System.Drawing.Size(78, 18);
+            this.lollipopLabel7.TabIndex = 6;
+            this.lollipopLabel7.Text = "城管类地图";
+            // 
+            // lollipopLabel8
+            // 
+            this.lollipopLabel8.AutoSize = true;
+            this.lollipopLabel8.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel8.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel8.Location = new System.Drawing.Point(1088, 89);
+            this.lollipopLabel8.Name = "lollipopLabel8";
+            this.lollipopLabel8.Size = new System.Drawing.Size(78, 18);
+            this.lollipopLabel8.TabIndex = 7;
+            this.lollipopLabel8.Text = "社管类地图";
+            // 
+            // lollipopLabel9
+            // 
+            this.lollipopLabel9.AutoSize = true;
+            this.lollipopLabel9.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel9.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel9.Location = new System.Drawing.Point(1201, 89);
+            this.lollipopLabel9.Name = "lollipopLabel9";
+            this.lollipopLabel9.Size = new System.Drawing.Size(50, 18);
+            this.lollipopLabel9.TabIndex = 8;
+            this.lollipopLabel9.Text = "其他类";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::iCarto.Properties.Resources.交通1综合图;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 134);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::iCarto.Properties.Resources.交通3主干线;
+            this.pictureBox2.Location = new System.Drawing.Point(195, 134);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::iCarto.Properties.Resources.测绘4地形图50000;
+            this.pictureBox3.Location = new System.Drawing.Point(391, 134);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::iCarto.Properties.Resources.国土1地表覆盖;
+            this.pictureBox4.Location = new System.Drawing.Point(582, 134);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::iCarto.Properties.Resources.交通1综合图;
+            this.pictureBox5.Location = new System.Drawing.Point(776, 134);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::iCarto.Properties.Resources.规划1城区;
+            this.pictureBox6.Location = new System.Drawing.Point(965, 134);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::iCarto.Properties.Resources.城管1网格图;
+            this.pictureBox7.Location = new System.Drawing.Point(391, 362);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::iCarto.Properties.Resources.社管1智慧社管网格图;
+            this.pictureBox8.Location = new System.Drawing.Point(582, 362);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 16;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::iCarto.Properties.Resources.警用1监控电子眼分布图;
+            this.pictureBox9.Location = new System.Drawing.Point(776, 362);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 17;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::iCarto.Properties.Resources.其他图1空气质量图;
+            this.pictureBox10.Location = new System.Drawing.Point(965, 362);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 18;
+            this.pictureBox10.TabStop = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 462);
-            this.Controls.Add(this.lollipopCard1);
-            this.Controls.Add(this.axMapControl1);
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lollipopLabel9);
+            this.Controls.Add(this.lollipopLabel8);
+            this.Controls.Add(this.lollipopLabel7);
+            this.Controls.Add(this.lollipopLabel6);
+            this.Controls.Add(this.lollipopLabel5);
+            this.Controls.Add(this.lollipopLabel4);
+            this.Controls.Add(this.lollipopLabel3);
+            this.Controls.Add(this.lollipopLabel2);
+            this.Controls.Add(this.lollipopLabel1);
             this.Name = "MainForm";
-            this.Text = "MainForm";
-            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.Text = "快速制图";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
-        private LollipopCard lollipopCard1;
+        private LollipopLabel lollipopLabel1;
+        private LollipopLabel lollipopLabel2;
+        private LollipopLabel lollipopLabel3;
+        private LollipopLabel lollipopLabel4;
+        private LollipopLabel lollipopLabel5;
+        private LollipopLabel lollipopLabel6;
+        private LollipopLabel lollipopLabel7;
+        private LollipopLabel lollipopLabel8;
+        private LollipopLabel lollipopLabel9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+
     }
 }
 
--- iCarto/MainForm.cs
+++ iCarto/MainForm.cs
@@ -6,14 +6,25 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using MaterialSkin.Controls;
+using MaterialSkin;
 
 namespace iCarto
 {
     public partial class MainForm : MaterialForm
     {
+        private readonly MaterialSkinManager materialSkinManager;
+
         public MainForm()
         {
             InitializeComponent();
+
+            //¸ü¸ÄÑÕÉ«
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green600, Primary.Green700, Primary.Green200, Accent.Red100, TextShade.WHITE);
+
+
         }
     }
 }
